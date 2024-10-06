a8c39782a011b4d4bbaf3b7af334bf0b7ca1fab6
gimmin-u <kimlg2015@naver.com>
2024-10-07 04:38:29 +0900

    타임워치 아이콘 버튼 삽입

--- lib/timewatch_screen.dart
+++ lib/timewatch_screen.dart
@@ -11,6 +11,34 @@ class TimewatchScreen extends StatefulWidget {
 class _TimewatchScreenState extends State<TimewatchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('타임워치'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
