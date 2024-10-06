f1fbde0bb0778d9c00c92732b2185a41eb4f7d58
gimmin-u <kimlg2015@naver.com>
2024-10-07 03:52:07 +0900

    타임워치 앱(fullter)

---
+++ android/app/src/main/kotlin/com/example/timewatch_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timewatch_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/timewatch_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart'; // 'matertal.dart'를 'material.dart'로 수정
+
+class TimewatchScreen extends StatefulWidget {
+  const TimewatchScreen({super.key});
+
+  @override
+  State<TimewatchScreen> createState() => _TimewatchScreenState(); // 문법 오류 수정
+}
+
+
+class _TimewatchScreenState extends State<TimewatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
