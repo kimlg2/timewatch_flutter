05463a5264bb25bf53bd9091b89cfc1816712ddd
gimmin-u <kimlg2015@naver.com>
2024-10-07 04:12:19 +0900

    타임워치 앱(fullter)

--- lib/timewatch_screen.dart
+++ lib/timewatch_screen.dart
@@ -7,6 +7,7 @@ class TimewatchScreen extends StatefulWidget {
   State<TimewatchScreen> createState() => _TimewatchScreenState(); // 문법 오류 수정
 }
 
+
 class _TimewatchScreenState extends State<TimewatchScreen> {
   @override
   Widget build(BuildContext context) {
